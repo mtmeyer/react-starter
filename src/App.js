b3ed2b9e0cb6e0179ab94701f6e5956ff94f7613
Harry Diamond <harrydiamond3@gmail.com>
2020-09-19 15:18:40 +1000

    1.0.0

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './App.module.scss';
+
+export default function App() {
+  return (
+    <div className={styles.App}>
+      <h1>React Starter</h1>
+    </div>
+  );
+}
